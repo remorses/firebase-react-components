e705d3337e7d67e0b9fe2ebabb44f72eeac8536d
remorses <beats.by.morse@gmail.com>
2020-05-24 13:15:30 +0200

    more test

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import { GoogleButton, GithubButton, AuthProvider, useAuthData } from '../src/'
+import { ThemeProvider, Box } from '@chakra-ui/core'
 import React, { useState, useContext } from 'react'
-import { H1, Image, Text, Box, Row } from 'hybrid-components'
 import firebase from 'firebase/app'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
@@ -9,27 +9,39 @@ const App = () => {
         firebase.initializeApp(firebaseConfig)
     }
     return (
-        <Box alignContent='center' alignItems='center'>
-            <AuthProvider
-                noPersistence
-                onLogin={async (user, creds) => {
-                    console.log(creds.toJSON())
-                }}
-                onError={(e) => alert(e.message)}
-            >
-                <GoogleButton
-                    text='Start With Google'
-                    // scopes={['https://www.googleapis.com/auth/cloud-platform']}
-                />
-                <GithubButton text='Start With Github' />
-                <DisplayUser />
-            </AuthProvider>
-        </Box>
+        <ThemeProvider>
+            <Box alignContent='center' alignItems='center'>
+                <AuthProvider
+                    noPersistence
+                    onLogin={async (user, creds) => {
+                        console.log('onLogin 1')
+                        console.log(creds.toJSON())
+                    }}
+                    onError={(e) => alert(e.message)}
+                >
+                    <AuthProvider
+                        noPersistence
+                        onLogin={async (user, creds) => {
+                            console.log('onLogin 2')
+                            console.log(creds.toJSON())
+                        }}
+                        onError={(e) => alert(e.message)}
+                    >
+                        <GoogleButton
+                            text='Start With Google'
+                            // scopes={['https://www.googleapis.com/auth/cloud-platform']}
+                        />
+                        <GithubButton text='Start With Github' />
+                        <DisplayUser />
+                    </AuthProvider>
+                </AuthProvider>
+            </Box>
+        </ThemeProvider>
     )
 }
 
 const DisplayUser = () => {
-    const { user={}, loading } = useAuthData()
+    const { user = {}, loading } = useAuthData()
     if (loading) {
         return <>loading...</>
     }
